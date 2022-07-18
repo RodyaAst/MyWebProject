2349e6d8ee463a3e4a6cb5e6e3d1bfb7ef510d09
Astafjev Rodion <rodion.astafev@informicus.ru>
2022-07-18 20:01:36 +0300

    add DrugSolver

--- Java/src/main/java/com/example/java/auth/SecurityConfig.java
+++ Java/src/main/java/com/example/java/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.java.auth;
 import com.example.java.auth.service.PersonAuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,6 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personAuthService);
     }
+
+    //TODO хотфикс доступа до POST-методов
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
